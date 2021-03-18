143c363fe4ef4e81eec7aefc780cc8385ee6c828
acasax <acasax@gmail.com>
2021-03-18 01:09:59 +0100

    fix light version

---
+++ radna_verzija/alpha_verzija/about.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!--================ Basic page needs ================-->
+  <title>Pollock | o hotelu</title>
+  <meta charset="UTF-8">
+  <meta name="author" content="">
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <!--================ Mobile specific metas ================-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!--================ Favicon ================-->
+  <link rel="shortcut icon" href="images/favicon.ico">
+  <!--================ Google web fonts ================-->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Old+Standard+TT:400,400i,700|Open+Sans:200,300,400,400i,600,600i,700,700i,800,800i|Playfair+Display:400,400i,700,700i,900,900i|Frank+Ruhl+Libre:500">
+  <!--================ Vendor CSS ================-->
+  <link rel="stylesheet" href="css/animate.min.css">
+  <link rel="stylesheet" href="css/fontawesome-all.min.css">
+  <link rel="stylesheet" href="css/linearicons.css">
+  <link rel="stylesheet" href="css/milenia-icon-font.css">
+  <link rel="stylesheet" href="vendors/owl-carousel/assets/owl.carousel.min.css">
+  <link rel="stylesheet" href="vendors/fancybox/jquery.fancybox.min.css">
+  <link rel="stylesheet" href="vendors/arcticmodal/jquery.arcticmodal-0.3.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!--================ Theme CSS ================-->
+  <link rel="stylesheet" href="css/style.css">
+  <!--================ Vendor JS ================-->
+</head>
+
+<body class="milenia-body--scheme-lightbrown milenia-body--border-layout">
+  <?php include "php_vendors/elements/navbar.php";   ?>
+  <!--================ End of Header ================-->
+ <!-- <div class="milenia-section milenia-section--stretched-content-no-px milenia-section--no-py">
+    <div class="owl-carousel owl-carousel--nav-edges owl-carousel--nav-huge owl-carousel--nav-inside owl-carousel--nav-hover-white milenia-simple-slideshow"><img src="images/slide-18.jpg" alt=""><img src="images/slide-19.jpg" alt=""><img src="images/slide-20.jpg" alt=""></div>
+  </div> -->
+
+  <div class="milenia-content  ">
+    <div class="container">
+      <!--================ Content Section ================-->
+      <section class="milenia-section milenia-section--stretched milenia-colorizer--scheme-lightest">
+        <div class="row align-items-center">
+          <div class="col-lg-6">
+            <h2 class="milenia-section-title milenia-section-title--style-2">Činimo obično, izvanredno.</h2>
+            <p class="milenia-section-description milenia-section-description--style-2">Aenean auctor wisi et urna. Aliquam erat volutpat. Duis ac turpis.</p>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris ferme ntum dictum magna. Sed laoreet aliquam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit. Aenean auctor wisi et urna. Aliquam erat volutpat. Duis ac turpis. </p>
+            <p>Integer rutrum ante ue lacus. Vestibulum libero nisl, porta vel, scelerisque eget, malesuada at, neque. Vivamus eget nibh. Etiam cursus leo vel metus. Nulla facilisi. Aenean nec eros. Vestibulum ante ipsum primis in faucibus.</p>
+          </div>
+          <div class="col-lg-6"><img src="images/about-image-1.jpg" alt=""></div>
+        </div>
+      </section>
+      <!--================ End of Content Section ================-->
+      <!--================ Content Section ================-->
+
+      <!--================ End of Content Section ================-->
+      <!--================ Content Section ================-->
+      <div class="milenia-section milenia-section--stretched-content-no-px milenia-section--no-py">
+        <!--================ Entities (Style 3) ================-->
+        <div class="milenia-entities milenia-entities--style-3 milenia-entities--filled milenia-entities--title-reduced">
+          <div class="milenia-grid milenia-grid--cols-1">
+            <div class="milenia-grid-item">
+              <!--================ Entity ================-->
+              <article class="milenia-entity milenia-entity--format-image">
+                <div class="milenia-entity-media"><a href="#" aria-labelledby="entity-heading-2" data-bg-image-src="images/about-image-3.jpg" class="milenia-entity-link milenia-ln--independent"></a></div>
+                <div class="milenia-entity-content milenia-aligner">
+                  <div class="milenia-aligner-outer">
+                    <div class="milenia-aligner-inner">
+                      <header class="milenia-entity-header">
+                        <h2 id="entity-heading-2" class="milenia-entity-title"><a href="#" class="milenia-color--unchangeable">Naša istorija</a></h2>
+                      </header>
+                      <div class="milenia-entity-body">
+                        <p>Donec porta diam eu massa. Quisque diam lorem, interdum vitae, dapibus ac, scelerisque vitae, pede. Donec eget tellus non erat lacinia fermentum. Donec in velit vel ipsum auctor pulvinar.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </article>
+              <!--================ End of Entity ================-->
+            </div>
+          </div>
+        </div>
+        <!--================ End of Entities (Style 3) ================-->
+      </div>
+      <!--================ End of Content Section ================-->
+      <!--================ Content Section ================-->
+      <div class="milenia-section">
+        <!--================ Entities (Style 5) ================-->
+        <div class="milenia-entities milenia-entities--style-5">
+          <div class="milenia-grid milenia-grid--cols-3">
+            <div class="milenia-grid-item">
+              <!--================ Entity ================-->
+              <article class="milenia-entity milenia-entity--format-image">
+                <div class="milenia-entity-media"><a href="#" aria-labelledby="entity-heading-10" class="milenia-ln--independent"><img src="images/info-box-image-11.jpg" alt=""></a></div>
+                <div class="milenia-entity-content milenia-aligner">
+                  <div class="milenia-aligner-outer">
+                    <div class="milenia-aligner-inner">
+                      <header class="milenia-entity-header">
+                        <h2 id="entity-heading-10" class="milenia-entity-title"><a href="#" class="milenia-color--unchangeable">Bazen</a></h2>
+                      </header>
+                      <div class="milenia-entity-body">
+                        <p>Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit. Aenean auctor wisi et urna. Aliquam erat volutpat. Duis ac turpis.</p>
+                      </div>
+                      <footer class="milenia-entity-footer"><a href="#" class="milenia-btn milenia-btn--link milenia-btn--scheme-primary">Pogledaj više</a></footer>
+                    </div>
+                  </div>
+                </div>
+              </article>
+              <!--================ End of Entity ================-->
+            </div>
+            <div class="milenia-grid-item">
+              <!--================ Entity ================-->
+              <article class="milenia-entity milenia-entity--format-image">
+                <div class="milenia-entity-media"><a href="#" aria-labelledby="entity-heading-11" class="milenia-ln--independent"><img src="images/info-box-image-12.jpg" alt=""></a></div>
+                <div class="milenia-entity-content milenia-aligner">
+                  <div class="milenia-aligner-outer">
+                    <div class="milenia-aligner-inner">
+                      <header class="milenia-entity-header">
+                        <h2 id="entity-heading-11" class="milenia-entity-title"><a href="#" class="milenia-color--unchangeable">Konferenciski prostor</a></h2>
+                      </header>
+                      <div class="milenia-entity-body">
+                        <p>Integer rutrum ante eu lacus. Vestibulum libero nisl, porta vel, scelerisque eget, malesuada at, neque. Vivamus eget nibh.</p>
+                      </div>
+                      <footer class="milenia-entity-footer"><a href="#" class="milenia-btn milenia-btn--link milenia-btn--scheme-primary">Pogledaj više</a></footer>
+                    </div>
+                  </div>
+                </div>
+              </article>
+              <!--================ End of Entity ================-->
+            </div>
+            <div class="milenia-grid-item">
+              <!--================ Entity ================-->
+              <article class="milenia-entity milenia-entity--format-image">
+                <div class="milenia-entity-media"><a href="#" aria-labelledby="entity-heading-12" class="milenia-ln--independent"><img src="images/info-box-image-13.jpg" alt=""></a></div>
+                <div class="milenia-entity-content milenia-aligner">
+                  <div class="milenia-aligner-outer">
+                    <div class="milenia-aligner-inner">
+                      <header class="milenia-entity-header">
+                        <h2 id="entity-heading-12" class="milenia-entity-title"><a href="#" class="milenia-color--unchangeable">Besplatan parking</a></h2>
+                      </header>
+                      <div class="milenia-entity-body">
+                        <p>Vivamus eget nibh. Etiam cursus leo vel metus. Nulla facilisi. Aenean nec eros. Vestibulum ante ipsum primis in faucibus orci luctus et.</p>
+                      </div>
+                      <footer class="milenia-entity-footer"><a href="#" class="milenia-btn milenia-btn--link milenia-btn--scheme-primary">Pogledaj više</a></footer>
+                    </div>
+                  </div>
+                </div>
+              </article>
+              <!--================ End of Entity ================-->
+            </div>
+          </div>
+        </div>
+        <!--================ End of Entities (Style 5) ================-->
+        <footer class="text-center"><a href="#" class="milenia-btn">See All Activities</a></footer>
+      </div>
+      <!--================ End of Content Section ================-->
+      <!--================ Content Section ================-->
+      <!--================ End of Content Section ================-->
+      <!--================ Content Section ================-->
+      <div class="milenia-section milenia-section--no-py milenia-section--stretched-content-no-px">
+        <!--================ Gallery ================-->
+        <div class="milenia-instafeed">
+          <div id="instafeed" data-get="user" data-user="customer" data-limit="6" class="milenia-grid milenia-grid--no-gutters milenia-grid--cols-6"></div>
+        </div>
+        <!--================ End of Gallery ================-->
+      </div>
+      <!--================ End of Content Section ================-->
+    </div>
+  </div>
+  <!--================ Footer ================-->
+  <?php include "php_vendors/elements/footer.php"; ?>
+  <!--================ End of Footer ================-->
+  </div>
+  <script src="vendors/modernizr.js"></script>
+  <script src="vendors/jquery-3.3.1.min.js"></script>
+  <script src="vendors/nicescroll/jquery.nicescroll.min.js"></script>
+  <script src="vendors/jquery.easing.1.3.min.js"></script>
+  <script src="vendors/instafeed.min.js"></script>
+  <script src="vendors/instafeed.wrapper.min.js"></script>
+  <script src="vendors/jquery.parallax-1.1.3.min.js"></script>
+  <script src="vendors/monkeysan.sameheight.min.js"></script>
+  <script src="vendors/isotope.pkgd.min.js"></script>
+  <script src="vendors/monkeysan.jquery.nav.1.0.min.js"></script>
+  <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+  <script src="vendors/arcticmodal/jquery.arcticmodal-0.3.min.js"></script>
+  <script src="vendors/fancybox/jquery.fancybox.min.js"></script>
+  <script src="vendors/monkeysan.validator.min.js"></script>
+  <script src="vendors/handlebars-v4.0.5.min.js"></script>
+  <script src="vendors/mad.customselect.js"></script>
+  <script src="vendors/retina.min.js"></script>
+  <script src="js/modules/milenia.sidebar-hidden.min.js"></script>
+  <script src="js/modules/milenia.newsletter-form.min.js"></script>
+  <script src="js/modules/milenia.alert-box.min.js"></script>
+  <script src="js/modules/milenia.isotope.js"></script>
+  <script src="js/modules/milenia.tabbed-grid.min.js"></script>
+  <script src="js/modules/milenia.sticky-header-section.min.js"></script>
+  <script src="js/milenia.app.js"></script>
+</body>
+
+</html>
